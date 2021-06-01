e675d3e210eb0c1136998e31ee1c3058ae18a242
Jack Harner <jack@harnerdesigns.com>
2021-06-01 09:19:41 -0600

    query fix

--- src/components/seo.js
+++ src/components/seo.js
@@ -67,7 +67,7 @@ const query = graphql`
         defaultTitle: title
         titleTemplate
         defaultDescription: description
-        siteUrl: url
+        siteUrl
         defaultImage: image
         twitterUsername
       }
